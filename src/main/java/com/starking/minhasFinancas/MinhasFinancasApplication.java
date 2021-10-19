f918c4a96c9e0b7cbc60a47d43df967d6c1ccd77
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-18 21:38:10 -0300

    criação de projeto

---
+++ src/main/java/com/starking/minhasFinancas/MinhasFinancasApplication.java
@@ -0,0 +1,13 @@
+package com.starking.minhasFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starking/minhasFinancas/MinhasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.minhasFinancas;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
