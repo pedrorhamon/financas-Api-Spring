dd0351f5c18b09c5fb8ff0f0bdb7a22824d4ad79
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-19 21:00:12 -0300

    mapeando a entitidade usuario

---
+++ src/main/java/com/starking/minhasFinancas/model/entity/Usuario.java
@@ -0,0 +1,30 @@
+package com.starking.minhasFinancas.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuario", schema = "financas")
+public class Usuario {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+}
