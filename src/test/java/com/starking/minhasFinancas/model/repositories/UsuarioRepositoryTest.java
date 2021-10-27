6e0a58858ff98e89ddbba881202c3094165cfb9e
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-26 21:10:18 -0300

    Finalizando testes Usuario

--- src/test/java/com/starking/minhasFinancas/model/repositories/UsuarioRepositoryTest.java
+++ src/test/java/com/starking/minhasFinancas/model/repositories/UsuarioRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.starking.minhasFinancas.model.repositories;
 
+import java.util.Optional;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,9 +29,7 @@ public class UsuarioRepositoryTest {
 
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
-		Usuario usuario = Usuario.builder()
-				.nome("pedro")
-				.email("pedro@gmail.com").build();
+		Usuario usuario = criarUsuario();
 		entityManager.persist(usuario);
 
 		boolean result = repository.existsByEmail("pedro@gmail.com");
@@ -43,5 +43,41 @@ public class UsuarioRepositoryTest {
 		boolean result = repository.existsByEmail("pedro@gmail.com");
 		
 		Assertions.assertThat(result).isFalse();	
+	}
+	
+	@Test
+	public void devePersistirUmUsuarionaBase() {
+		Usuario usuario = criarUsuario();
+		Usuario usuarioSalvar = repository.save(usuario);
+		
+		Assertions.assertThat(usuarioSalvar.getId()).isNotNull();
+	}
+	
+	@Test
+	public void deveBuscarUsuarioEmail() {
+		Usuario usuario = criarUsuario();
+		
+		entityManager.persist(usuario);
+		
+		Optional<Usuario> result = repository.findByEmail("pedro@gmail.com");
+		
+		Assertions.assertThat(result.isPresent()).isTrue();
+	}
+	
+	@Test
+	public void deveRetornarVazioEmail() {
+		
+		Optional<Usuario> result = repository.findByEmail("pedro@gmail.com");
+		
+		Assertions.assertThat(result.isPresent()).isFalse();
+	}
+	
+	public static Usuario criarUsuario() {
+		return Usuario 
+				.builder()
+				.nome("pedro")
+				.email("pedro@gmail.com")
+				.senha("123456")
+				.build();
 	}
 }
