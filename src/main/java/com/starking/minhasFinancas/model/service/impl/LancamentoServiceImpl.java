39fd9a6c81f96d9c59679450c8c686bfe806ced7
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-04 21:12:22 -0300

    metodo Salvar lancamento

--- src/main/java/com/starking/minhasFinancas/model/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/starking/minhasFinancas/model/service/impl/LancamentoServiceImpl.java
@@ -3,18 +3,26 @@ package com.starking.minhasFinancas.model.service.impl;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.starking.minhasFinancas.model.entity.Lancamento;
 import com.starking.minhasFinancas.model.enums.StatusLancamento;
+import com.starking.minhasFinancas.model.repositories.LancamentoRepository;
 import com.starking.minhasFinancas.model.service.LancamentoService;
 
 @Service
 public class LancamentoServiceImpl implements LancamentoService{
 
+	private LancamentoRepository lancamentoRepository;
+	
+	public LancamentoServiceImpl(LancamentoRepository lancamentoRepository) {
+		this.lancamentoRepository = lancamentoRepository;
+	}
+	
 	@Override
+	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		return lancamentoRepository.save(lancamento);
 	}
 
 	@Override
