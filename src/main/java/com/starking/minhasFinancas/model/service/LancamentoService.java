b4ae4e61dff962a27188dc5eef9a66e40bb82c15
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-04 21:09:44 -0300

    Criando o Serviço de lancamento

---
+++ src/main/java/com/starking/minhasFinancas/model/service/LancamentoService.java
@@ -0,0 +1,16 @@
+package com.starking.minhasFinancas.model.service;
+
+import java.util.List;
+
+import com.starking.minhasFinancas.model.entity.Lancamento;
+import com.starking.minhasFinancas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+	
+	Lancamento salvar(Lancamento lancamento);
+	Lancamento atualizar(Lancamento lancamento);
+	void deletar(Lancamento lancamento);
+	List<Lancamento> buscar(Lancamento lancamentoFiltro);
+	void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento);
+
+}
---
+++ src/main/java/com/starking/minhasFinancas/model/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.starking.minhasFinancas.model.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.starking.minhasFinancas.model.entity.Lancamento;
+import com.starking.minhasFinancas.model.enums.StatusLancamento;
+import com.starking.minhasFinancas.model.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService{
+
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
