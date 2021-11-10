8ed27574f6a0e0475b0e89a1d54da5b36614008e
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-10 20:51:25 -0300

    Implementando os endpoints de salvar e atualizar lancamentos

--- src/main/java/com/starking/minhasFinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/starking/minhasFinancas/api/resource/LancamentoResource.java
@@ -1,7 +1,10 @@
 package com.starking.minhasFinancas.api.resource;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,18 +23,40 @@ import com.starking.minhasFinancas.model.service.UsuarioService;
 public class LancamentoResource {
 
 	private LancamentoService lancamentoService;
-	
+
 	private UsuarioService usuarioService;
 
 	public LancamentoResource(LancamentoService lancamentoService) {
 		this.lancamentoService = lancamentoService;
 	}
-	
+
 	@PostMapping
 	public ResponseEntity salvar(@RequestBody LancamentoDto lancamentoDto) {
-		return null;	
+		try {
+			Lancamento entidade = converter(lancamentoDto);
+			entidade = lancamentoService.salvar(entidade);
+			return new ResponseEntity(entidade, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
-	
+
+	@PutMapping("{id}")
+	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDto lancamentoDto) {
+		return lancamentoService.obterPorId(id).map(entity -> {
+			try {
+				Lancamento lancamento = converter(lancamentoDto);
+				lancamento.setId(entity.getId());
+				lancamentoService.atualizar(lancamento);
+				return ResponseEntity.ok(lancamento);
+				
+			} catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}).orElseGet(() -> 
+		new ResponseEntity("Lancamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+	}
+
 	private Lancamento converter(LancamentoDto lancamentoDto) {
 		Lancamento lancamento = new Lancamento();
 		lancamento.setId(lancamentoDto.getId());
@@ -39,14 +64,14 @@ public class LancamentoResource {
 		lancamento.setAno(lancamentoDto.getAno());
 		lancamento.setMes(lancamentoDto.getMes());
 		lancamento.setValor(lancamentoDto.getValor());
-		
+
 		Usuario usuario = usuarioService.obterPorId(lancamentoDto.getUsuario())
-		.orElseThrow( () -> new RegraNegocioException("Usuario não encontrado para o Id informado. "));
-		
+				.orElseThrow(() -> new RegraNegocioException("Usuario não encontrado para o Id informado. "));
+
 		lancamento.setUsuario(usuario);
 		lancamento.setTipo(TipoLancamento.valueOf(lancamentoDto.getTipo()));
 		lancamento.setStatus(StatusLancamento.valueOf(lancamentoDto.getStatus()));
-		
+
 		return lancamento;
 	}
 }
--- src/main/java/com/starking/minhasFinancas/model/service/LancamentoService.java
+++ src/main/java/com/starking/minhasFinancas/model/service/LancamentoService.java
@@ -1,17 +1,25 @@
 package com.starking.minhasFinancas.model.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.starking.minhasFinancas.model.entity.Lancamento;
 import com.starking.minhasFinancas.model.enums.StatusLancamento;
 
 public interface LancamentoService {
-	
+
 	Lancamento salvar(Lancamento lancamento);
+
 	Lancamento atualizar(Lancamento lancamento);
+
 	void deletar(Lancamento lancamento);
+
 	List<Lancamento> buscar(Lancamento lancamentoFiltro);
+
 	void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento);
+
 	void validar(Lancamento lancamento);
+	
+	Optional<Lancamento> obterPorId(Long id);
 
 }
