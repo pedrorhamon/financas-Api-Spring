36da85f6aed3d5cfc85e94be1cafea2c3f0147d0
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-12 23:42:34 -0300

    Finalizando os testes de Integração do Repositório de Lancamentos

--- src/test/java/com/starking/minhasFinancas/model/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/starking/minhasFinancas/model/repositories/LancamentoRepositoryTest.java
@@ -2,6 +2,7 @@ package com.starking.minhasFinancas.model.repositories;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -49,6 +50,38 @@ public class LancamentoRepositoryTest {
 		
 		Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
 		Assertions.assertThat(lancamentoInexistente).isNull();
+	}
+	
+	@Test
+	public void deveAtualizarLancamento() {
+		Lancamento lancamento = criarPersistirLancamento();
+		
+		lancamento.setAno(2018);
+		lancamento.setDescricao("Testes");
+		lancamento.setStatus(StatusLancamento.CANCELADO);
+		
+		repository.save(lancamento);
+		
+		Lancamento lancamentoAtualizado = entityManager.find(Lancamento.class, lancamento.getId());
+		
+		Assertions.assertThat(lancamentoAtualizado.getAno()).isEqualTo(2018);
+		Assertions.assertThat(lancamentoAtualizado.getDescricao()).isEqualTo("Testes");
+		Assertions.assertThat(lancamentoAtualizado.getStatus()).isEqualTo(StatusLancamento.CANCELADO);
+	}
+	
+	@Test
+	public void deveBuscarPorId() {
+		Lancamento lancamento = criarPersistirLancamento();
+		
+		Optional<Lancamento> lancamentoEncontrado = repository.findById(lancamento.getId());
+		
+		Assertions.assertThat(lancamentoEncontrado.isPresent()).isTrue();
+	}
+	
+	private Lancamento criarPersistirLancamento() {
+		Lancamento lancamento = criarLancamento();
+		entityManager.persist(lancamento);
+		return lancamento;
 	}
 	
 	private Lancamento criarLancamento() {
