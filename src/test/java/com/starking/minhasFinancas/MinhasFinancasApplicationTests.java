b650bfc1bd18eafbf2d66e76f524e1cf3c3f5392
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-11 18:27:05 -0300

    Alteraçoes no application testes

--- src/test/java/com/starking/minhasFinancas/MinhasFinancasApplicationTests.java
+++ src/test/java/com/starking/minhasFinancas/MinhasFinancasApplicationTests.java
@@ -1,9 +1,16 @@
 package com.starking.minhasFinancas;
 
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class MinhasFinancasApplicationTests {
-
+	
+	@Test
+	public void contextLoads() {
+	}
 }
