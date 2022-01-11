e1c078433cada87ca127e7c432977c8c7a6b15e5
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-11 18:25:40 -0300

    alterações

--- src/test/java/com/starking/minhasFinancas/MinhasFinancasApplicationTests.java
+++ src/test/java/com/starking/minhasFinancas/MinhasFinancasApplicationTests.java
@@ -1,14 +1,9 @@
 package com.starking.minhasFinancas;
 
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MinhasFinancasApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
